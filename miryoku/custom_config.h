e6817f7a935b7b4ab968d3216707b212609c351f
Konstantinos Chousos <79325777+kchousos@users.noreply.github.com>
2026-02-15 16:12:31 +0200

    Add configuration defines for Miryoku layout

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,3 +6,12 @@
 &kp F11,           &kp F4,            &kp F5,            &kp F6,            &kp SLCK,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &kp F10,           &kp F1,            &kp F2,            &kp F3,            &kp PAUSE_BREAK,   U_NA,              &u_to_U_FUN,       &u_to_U_MEDIA,     &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp LGUI,         &kp SPACE,         &kp TAB,           U_NA,              U_NA,              U_NA,              U_NP,              U_NP
+
+#define MIRYOKU_ALPHAS_QWERTY
+
+// https://github.com/kchousos/miryoku_zmk?tab=readme-ov-file#tap-delay
+#define MIRYOKU_KLUDGE_TAPDELAY
+
+#define U_TAPPING_TERM 200
+
+#define MIRYOKU_KLUDGE_SOFT_OFF
